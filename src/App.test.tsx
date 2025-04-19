e00041e991fce726d39cd5d93139175a653b7755
Joakim <joakim@hedberg.xyz>
2025-04-19 15:55:23 +0200

    Just adjusted som test cases, aint much

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders uppdragsledare input box', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByPlaceholderText('Uppdragsledare')
+  expect(element).toBeInTheDocument();
 });
+
+test('Renders the copy button', () => {
+  render(<App />)
+  const element = screen.getByText('Kopiera')
+  expect(element).toBeInTheDocument()
+})
